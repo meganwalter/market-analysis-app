ce469094ffa9028c4efd5c05e69d858b2d0c2097
Megan Walter <megwalter@hotmail.com>
2016-02-05 02:24:36 -0500

    got the event listener working and the counters to work

--- script/app.js
+++ script/app.js
@@ -1,13 +1,14 @@
 var picsArr = [];
 var names = ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine_glass"];
 
+//constructor function//
 function Product(name) {
   this.path = "images/" + name + ".jpg";
   this.name = name;
   this.counter = 0;
   picsArr.push(this);//push to array
 }
-
+//making my objects from the array//
 function makePics() {
   for(var name in names) {
     new Product(names[name]);
@@ -20,24 +21,55 @@ var tracker = {
   picOne: document.getElementById('choiceOne'),
   picTwo: document.getElementById('choiceTwo'),
   picThree: document.getElementById('choiceThree'),
+  allPics: document.getElementById('allImages'),
   randomImg: function() {
-  return Math.floor(Math.random() * names.length);
+    return Math.floor(Math.random() * names.length);
   },
 
-  displayPics: function(event) {
-    tracker.picOne.src = picsArr[tracker.randomImg()].path;
-    tracker.picTwo.src = picsArr[tracker.randomImg()].path;
-    tracker.picThree.src = picsArr[tracker.randomImg()].path;
+  displayPics: function(pic) { //add position - this will incorporate the PicOne, Two, Three above
+    pic.src = picsArr[tracker.randomImg()].path;
+    return pic.src;
   },
+
+  displayThreePics: function() {
+   var one = tracker.displayPics(tracker.picOne);
+   var two = tracker.displayPics(tracker.picTwo);
+   var three = tracker.displayPics(tracker.picThree);
+ },
+
+//imageClicked: function(event){
+//var elId = event.target.id;
+//var prod = tracker.findProduct(tracker.displayedProducts[elId]);
+
+//prod.functiontoincreasecounter();
+//code to update the 3 images
+//code to tell the computer which round the user is on (stops at 15)
+//function to check if the game is over
+//}
+
+//register event listeners function
+//function to duren off the event listeners "remove Event Listener"
 
   userChoice: function() {
-    addEventListener('click', function(){
-        tracker.displayPics();
+    tracker.allPics.addEventListener('click', function(event) {
+      if (event.target.tagName === 'IMG') {
+        for (x in picsArr) {
+          if (event.target.src.indexOf(picsArr[x].path) !== -1) {
+            picsArr[x].counter++;
+            console.log(picsArr[x].counter);
+          }
+        }
+
+        tracker.displayThreePics();
+      }
     }, false);
   },
 };
 
+//for loop, if this.id = Product.name then Product.counter++
+// this.id
+//tracker.object.counter++;
 window.onload = function() {
-  tracker.displayPics();
+  tracker.displayThreePics();
   tracker.userChoice();
 };
