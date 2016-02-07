f33b3c62245f625ef5f3ab2057f5861f41dfa0fb
Megan Walter <megwalter@hotmail.com>
2016-02-07 18:09:54 -0500

    got the game to stop after 15 rounds

--- script/app.js
+++ script/app.js
@@ -1,16 +1,17 @@
 var picsArr = [];
 var names = ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine_glass"];
+var rounds = 0;
+var maxRounds = 15;
 
-//constructor function//
 function Product(name) {
   this.path = "images/" + name + ".jpg";
   this.name = name;
   this.counter = 0;
   picsArr.push(this);//push to array
 }
-//making my objects from the array//
+
 function makePics() {
-  for(var name in names) {
+  for (var name in names) {
     new Product(names[name]);
   }
 }
@@ -44,26 +45,19 @@ var tracker = {
    }
  },
 
-//imageClicked: function(event){
-//var elId = event.target.id;
-//var prod = tracker.findProduct(tracker.displayedProducts[elId]);
-
-//prod.functiontoincreasecounter();
-//code to update the 3 images
-//code to tell the computer which round the user is on (stops at 15)
-//function to check if the game is over
-//}
-
-//register event listeners function
-//function to duren off the event listeners "remove Event Listener"
-
-  userChoice: function() {
-    tracker.allPics.addEventListener('click', function(event) {
+userChoice: function() {
+    tracker.allPics.addEventListener('click', function playGame(event) {
       if (event.target.tagName === 'IMG') {
         for (x in picsArr) {
           if (event.target.src.indexOf(picsArr[x].path) !== -1) {
             picsArr[x].counter++;
-            console.log(picsArr[x].counter);
+            rounds++;
+            console.log(rounds);
+            if (rounds === maxRounds) {
+              tracker.allPics.removeEventListener('click', playGame);
+              //show button
+              //run the chart function
+            }
           }
         }
 
@@ -73,9 +67,6 @@ var tracker = {
   },
 };
 
-//for loop, if this.id = Product.name then Product.counter++
-// this.id
-//tracker.object.counter++;
 window.onload = function() {
   tracker.displayThreePics();
   tracker.userChoice();
