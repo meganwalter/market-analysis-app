f7c4fb59dac77aa06767773acbd083991b5c1209
Megan Walter <megwalter@hotmail.com>
2016-02-07 22:12:34 -0500

    added a few notes and such

--- script/app.js
+++ script/app.js
@@ -44,8 +44,10 @@ var tracker = {
      }
    }
  },
-
-userChoice: function() {
+  submitAnswers: function() {
+    var button = document.getElementById('id')
+  }
+  userChoice: function() {
     tracker.allPics.addEventListener('click', function playGame(event) {
       if (event.target.tagName === 'IMG') {
         for (x in picsArr) {
@@ -56,7 +58,8 @@ userChoice: function() {
             if (rounds === maxRounds) {
               tracker.allPics.removeEventListener('click', playGame);
               //show button
-              //run the chart function
+              //run the chart function (submit click event will trigger)
+              //
             }
           }
         }
@@ -70,4 +73,27 @@ userChoice: function() {
 window.onload = function() {
   tracker.displayThreePics();
   tracker.userChoice();
+};
+
+var myBarChart = new Chart(ctx).Bar(data, options);
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
 };
