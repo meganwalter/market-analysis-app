06cc54e899d46d931e95a33d39005ecaadabc2ce
Megan Walter <megwalter@hotmail.com>
2016-02-08 22:09:35 -0500

    got chart js to populate with data from my game

--- script/app.js
+++ script/app.js
@@ -7,7 +7,7 @@ function Product(name) {
   this.path = "images/" + name + ".jpg";
   this.name = name;
   this.counter = 0;
-  picsArr.push(this);//push to array
+  picsArr.push(this);
 }
 
 function makePics() {
@@ -27,7 +27,7 @@ var tracker = {
     return Math.floor(Math.random() * names.length);
   },
 
-  displayPics: function(pic) { //add position - this will incorporate the PicOne, Two, Three above
+  displayPics: function(pic) {
     pic.src = picsArr[tracker.randomImg()].path;
     return pic.src;
   },
@@ -38,28 +38,29 @@ var tracker = {
      var two = tracker.displayPics(tracker.picTwo);
      var three = tracker.displayPics(tracker.picThree);
      if (one === two || two === three || one === three) {
-       console.log('one', one, 'two', two, 'three', three);
+       console.log('re-run');
      } else {
        return false;
      }
    }
  },
+
   submitAnswers: function() {
-    var button = document.getElementById('id')
-  }
+    var button = document.getElementById('id');
+  },
+
   userChoice: function() {
     tracker.allPics.addEventListener('click', function playGame(event) {
       if (event.target.tagName === 'IMG') {
         for (x in picsArr) {
           if (event.target.src.indexOf(picsArr[x].path) !== -1) {
             picsArr[x].counter++;
+            console.log(picsArr[x].counter);
             rounds++;
             console.log(rounds);
             if (rounds === maxRounds) {
               tracker.allPics.removeEventListener('click', playGame);
-              //show button
-              //run the chart function (submit click event will trigger)
-              //
+              tracker.buildBarChart();
             }
           }
         }
@@ -68,32 +69,33 @@ var tracker = {
       }
     }, false);
   },
+
+  buildBarChart: function() {
+    var data = {
+    labels: ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine_glass"],
+    datasets: [
+      {
+      label: "User Choices",
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data: [],
+    },
+  ],
+  };
+    for (var pics in picsArr) {
+      var picData = picsArr[pics].counter;
+      data.datasets[0].data.push(picData);
+      console.log(data.datasets[0].data);
+    }
+
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myNewChart = new Chart(ctx).Bar(data);
+  },
 };
 
 window.onload = function() {
   tracker.displayThreePics();
   tracker.userChoice();
-};
-
-var myBarChart = new Chart(ctx).Bar(data, options);
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
 };
